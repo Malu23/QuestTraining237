a184a87373ba4da5dc65a4e169b450ba6a7cab35
Malavika Bysakh <malavikabysakh@gmail.com>
2024-10-17 23:42:57 +0530

    Day 13 commit

---
+++ Day13_171024/Constructor/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Constructor
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string[] PhoneNumbers { get; set; }
+
+        public Person() // default constructor 
+        {
+            CreatedAt = DateTime.Now;
+            PhoneNumbers = new string[5];
+        }
+        
+        public Person(int PhoneNumbersCount) // parameterized constructor 
+        {
+            CreatedAt = DateTime.Now;
+            PhoneNumbers = new string[PhoneNumbersCount];
+        }
+
+        public Person(string name, int PhoneNumbersCount) 
+        {
+            Name = name;
+            CreatedAt = DateTime.Now;
+            PhoneNumbers = new string[PhoneNumbersCount];
+        }
+
+        public void PrintDetails()
+        {
+            Console.WriteLine($"Name: {Name ?? "Not provided"}");
+            Console.WriteLine($"Created At: {CreatedAt}");
+            Console.WriteLine("Phone Numbers:");
+
+            for (int i = 0; i < PhoneNumbers.Length; i++)
+            {
+                Console.WriteLine($"  [{i}] {PhoneNumbers[i] ?? "Not assigned"}");
+            }
+
+            Console.WriteLine();
+        }
+
+
+    }
+
+    internal class Program
+    {
+        static void Main()
+        {
+            var obj1 = new Person(); // default constructor call
+            var obj2 = new Person(2); // parameterized constructor call
+            var obj3 = new Person("Malu", 2);
+
+            Console.WriteLine("Details of obj1:");
+            obj1.PrintDetails();
+
+            Console.WriteLine("Details of obj2:");
+            obj2.PrintDetails();
+
+            Console.WriteLine("Details of obj3:");
+            obj3.PrintDetails();
+        }
+    }
+}
