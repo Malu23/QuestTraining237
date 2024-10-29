873e681d5d0fa4bf09fd725fa884e82281a20afb
Malavika Bysakh <malavikabysakh@gmail.com>
2024-10-29 23:30:27 +0530

    Day 20 commit

---
+++ Day20_281024/Threads/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Threads
+{
+    internal class Program
+    {
+        static void Action1()
+        {
+            Thread.Sleep(2000);
+            System.Console.WriteLine("Hello from Action1.");
+        }
+
+        static void Action2()
+        {
+            System.Console.WriteLine("Hello from Action2.");
+        }
+
+        static void Main(string[] args)
+        {
+            var t1 = new Thread(Action1);
+            var t2 = new Thread(Action2);
+            t1.Start();
+            t2.Start();
+            t1.Join();
+            t2.Join();
+            System.Console.WriteLine("COMPLETED");
+        }
+    }
+}
